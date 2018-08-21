3392addcdd9deee56475f80d732c8c394d8ad4dc
smiddeku <smiddeku@IN-MUM-LNDVM006.corp.capgemini.com>
2018-08-21 08:44:39 -0400

    Last commi

--- src/com/srinivas/day1/Absdiff.java
+++ src/com/srinivas/day1/Absdiff.java
@@ -1,7 +1,7 @@
 package com.srinivas.day1;
 
 import java.io.*;
-public class absdiff {
+public class Absdiff {
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
